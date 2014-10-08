5b2e83f86308bc898374a42f040dbabe17ce94de
sfernandezyedra6 <sfernandezyedra6@gmail.com>
2014-10-08 18:49:33 +0200

    Menú listo

---
+++ Assets/Scripts/gameScript.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gameScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+
+
+	void Update () {
+		if (Input.GetKey (KeyCode.Escape)) {
+			Reiniciar();
+				}
+
+	}
+
+
+	public void Salir (){
+		Application.Quit ();
+
+	}
+
+	public void Reiniciar () {
+		Application.LoadLevel ("menu");
+	
+	}
+
+	public void Iniciar () {
+		Application.LoadLevel ("nivel01");
+
+	}
+
+
+
+
+
+}
